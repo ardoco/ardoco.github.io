1f777035f345499d7c609e58dc54c16e861b25bc
dfuchss <develop@fuchss.org>
2025-08-04 20:50:39 +0000

    Deploying to gh-pages from @ ardoco/ardoco.github.io@dcc8b906d97c6e92a1f67b59d8e53533dd031a1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "dropdown-aire-2025",
+  },{id: "nav-approaches",
+          title: "approaches",
+          description: "Approaches within ARDoCo",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/approaches/";
+          },
+        },{id: "dropdown-aire-2025",
               title: "AIRE 2025",
               description: "",
               section: "Dropdown",
@@ -81,7 +88,7 @@ ninja.data = [{
               },
             },{id: "nav-publications",
           title: "publications",
-          description: "publications that are related to ArDoCo",
+          description: "publications that are related to ARDoCo",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
@@ -95,12 +102,107 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of ArDoCo",
+          description: "members of ARDoCo",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
           },
-        },{
+        },{id: "approaches-arcotl",
+          title: 'ArCoTL',
+          description: "ArCoTL – TLR between Software Architecture Models and Code.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/arcotl/";
+            },},{id: "approaches-ardocode",
+          title: 'ArDoCode',
+          description: "ArDoCode – TLR between Software Architecture Documentation and Code.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/ardocode/";
+            },},{id: "approaches-inconsistency-detection",
+          title: 'Inconsistency Detection',
+          description: "Documentation-Model-Inconsistency-Analysis pipeline.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/inconsistency-detection/";
+            },},{id: "approaches-lissa",
+          title: 'LiSSA',
+          description: "LiSSA – LLM/RAG-based TLR.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/lissa/";
+            },},{id: "approaches-secdragon",
+          title: 'SecDragon',
+          description: "SecDragon – TLR for Security Requirements.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/secdragon/";
+            },},{id: "approaches-swattr",
+          title: 'SWATTR',
+          description: "SWATTR – TLR between Software Architecture Documentation and Software Architecture Models.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/swattr/";
+            },},{id: "approaches-transarc",
+          title: 'TransArC',
+          description: "TransArC – TLR between Software Architecture Documentation, Models, and Code.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/transarc/";
+            },},{id: "approaches-transarc-ai",
+          title: 'TransArC-AI',
+          description: "TransArC-AI – LLM-based TLR between Software Architecture Documentation, Models, and Code.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/transarc-ai/";
+            },},{id: "approaches-ardoco-tv",
+          title: 'ARDoCo-TV',
+          description: "Trace View: a viewer for trace links.",
+          section: "Approaches",handler: () => {
+              window.location.href = "/approaches/tv/";
+            },},{id: "conferences-beyond-retrieval-a-study-of-using-llm-ensembles-for-candidate-filtering-in-requirements-traceability",
+          title: 'Beyond Retrieval: A Study of Using LLM Ensembles for Candidate Filtering in Requirements...',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/aire25";
+            },},{id: "conferences-trace-link-recovery-for-software-architecture-documentation",
+          title: 'Trace Link Recovery for Software Architecture Documentation',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/ecsa21";
+            },},{id: "conferences-llm-gestützte-softwarearchitektur-eine-neue-ära",
+          title: 'LLM-gestützte Softwarearchitektur: Eine neue Ära?',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/fg-arch24";
+            },},{id: "conferences-detecting-inconsistencies-in-software-architecture-documentation-using-traceability-link-recovery",
+          title: 'Detecting Inconsistencies in Software Architecture Documentation Using Traceability Link Recovery',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/icsa23";
+            },},{id: "conferences-enabling-architecture-traceability-by-llm-based-architecture-component-name-extraction",
+          title: 'Enabling Architecture Traceability by LLM-based Architecture Component Name Extraction',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/icsa25";
+            },},{id: "conferences-recovering-trace-links-between-software-documentation-and-code",
+          title: 'Recovering Trace Links Between Software Documentation And Code',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/icse24";
+            },},{id: "conferences-lissa-toward-generic-traceability-link-recovery-through-retrieval-augmented-generation",
+          title: 'LiSSA: Toward Generic Traceability Link Recovery through Retrieval-Augmented Generation',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/icse25";
+            },},{id: "conferences-requirements-traceability-link-recovery-via-retrieval-augmented-generation",
+          title: 'Requirements Traceability Link Recovery via Retrieval-Augmented Generation',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/refsq25";
+            },},{id: "conferences-detecting-inconsistencies-in-software-architecture-documentation-using-traceability-link-recovery",
+          title: 'Detecting Inconsistencies in Software Architecture Documentation Using Traceability Link Recovery',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/se24";
+            },},{id: "conferences-recovering-trace-links-between-software-documentation-and-code",
+          title: 'Recovering Trace Links Between Software Documentation And Code',
+          description: "",
+          section: "Conferences",handler: () => {
+              window.location.href = "/c/se25";
+            },},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
